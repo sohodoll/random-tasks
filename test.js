e8b2cce5a63525b435f3ddb0922b7139f86b6537
sohodoll <kovalevvtl@gmail.com>
2022-11-08 16:36:17 +0300

    feat: async practice

--- test.js
+++ test.js
@@ -1,21 +1,49 @@
-const { stdin, stdout } = process;
-const flag = process.argv[2];
+// const { stdin, stdout } = process;
+// const flag = process.argv[2];
 
-const calculate  = (a, b) => {
-    stdout.write('Type 2 numbers\n');
+// const calculate  = (a, b) => {
+//     stdout.write('Type 2 numbers\n');
 
-    stdin.on('data', data => {
-        const numString = data.toString();
-        const array = numString.split(' ');
-        let result;
-        if (flag == '-m') {
-            result = Number(array[0]) + Number(array[1]);
-        } else {
-            result = Number(array[0]) * Number(array[1]);
-        }
-        stdout.write(result.toString());
-        process.exit();
-    });
-}
+//     stdin.on('data', data => {
+//         const numString = data.toString();
+//         const array = numString.split(' ');
+//         let result;
+//         if (flag == '-m') {
+//             result = Number(array[0]) + Number(array[1]);
+//         } else {
+//             result = Number(array[0]) * Number(array[1]);
+//         }
+//         stdout.write(result.toString());
+//         process.exit();
+//     });
+// }
 
-calculate();
+// calculate();
+
+// const myPromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         reject(new Error());
+//     }, 1000);
+
+// }).then(data => {
+//     console.log(data);
+// })
+// .catch(err => {
+//     console.log(err);
+// })
+
+ setTimeout(() => {
+    console.log('hey1')
+ }, 1000);
+
+ setTimeout(() => {
+    console.log('hey1')
+}, 1000);
+
+setTimeout(() => {
+    console.log('hey1')
+}, 1000);
+
+setTimeout(() => {
+    console.log('hey1')
+}, 1000);
